848ffeae1f5a6889c70f0b46b33fb6a8d6e503ca
Avery Burne <burneav@gmail.com>
2022-08-18 14:40:44 -0400

    Took the pong game I had already and turned it 90 degrees

--- script.js
+++ script.js
@@ -8,27 +8,19 @@ var isDragging = false;
 var playerBlock;
 var computerBlock;
 // gameBoard.width = '10%'
-var Block;
-var leftBlock = {
-    x: 50,
-    y: 200,
-    width: 20,
-    height: 100,
-    incoming: false
-};
 var rightBlock = {
-    x: 750,
-    y: 200,
-    width: 20,
-    height: 100,
+    x: 350,
+    y: 450,
+    width: 100,
+    height: 20,
     incoming: true
 };
 var gameBall = {
     x: 250,
     y: 100,
     radius: 10,
-    dy: 0,
-    dx: 2
+    dy: 2,
+    dx: 0
 };
 var scores = {
     leftPlayerScore: 0,
@@ -46,9 +38,6 @@ function main() {
             setTimeout(function () {
                 draw();
             }, 3000);
-        }
-        if (isDragging) {
-            computerMove(computerBlock);
         }
         gameBall.x += gameBall.dx;
         gameBall.y += gameBall.dy;
@@ -68,43 +57,11 @@ var clearCanvas = function () {
 var draw = function () {
     gameBoardContext.fillStyle = 'lightblue';
     gameBoardContext.strokeStyle = 'darkblue';
-    gameBoardContext.fillRect(leftBlock.x, leftBlock.y, leftBlock.width, leftBlock.height);
-    gameBoardContext.strokeRect(leftBlock.x, leftBlock.y, leftBlock.width, leftBlock.height);
     gameBoardContext.fillRect(rightBlock.x, rightBlock.y, rightBlock.width, rightBlock.height);
     gameBoardContext.strokeRect(rightBlock.x, rightBlock.y, rightBlock.width, rightBlock.height);
     gameBoardContext.beginPath();
     gameBoardContext.arc(gameBall.x, gameBall.y, gameBall.radius, 0, 2 * Math.PI);
     gameBoardContext.stroke();
-};
-var changeDirection = function (e) {
-    var UP_KEY = 38;
-    var DOWN_KEY = 40;
-    var W_KEY = 87;
-    var S_KEY = 83;
-    var keyPressed = e.keyCode;
-    if (keyPressed === UP_KEY && rightBlock.y > 0) {
-        rightBlock.y -= 20;
-    }
-    else if (keyPressed === DOWN_KEY && rightBlock.y < (400 - rightBlock.height)) {
-        rightBlock.y += 20;
-    }
-    else if (keyPressed === W_KEY && leftBlock.y > 0) {
-        leftBlock.y -= 20;
-    }
-    else if (keyPressed === S_KEY && leftBlock.y < (400 - leftBlock.height)) {
-        leftBlock.y += 20;
-    }
-    clearCanvas();
-    draw();
-};
-var checkIfHitLeftBlock = function () {
-    if (((gameBall.x - gameBall.radius) <= (leftBlock.x + leftBlock.width) && (gameBall.x - gameBall.radius) >= (leftBlock.x))
-        && ((gameBall.y + gameBall.radius) > leftBlock.y && (gameBall.y - gameBall.radius) < (leftBlock.y + 100))) {
-        return true;
-    }
-    else {
-        return false;
-    }
 };
 var checkIfHitRightBlock = function () {
     if ((((gameBall.x + gameBall.radius) >= rightBlock.x) && ((gameBall.x + gameBall.radius) <= (rightBlock.x + rightBlock.width)))
@@ -147,33 +104,14 @@ var mouseDown = function (e) {
     if (checkIfInBlock(startX, startY, rightBlock)) {
         isDragging = true;
         playerBlock = rightBlock;
-        computerBlock = leftBlock;
-    }
-    else if (checkIfInBlock(startX, startY, leftBlock)) {
-        isDragging = true;
-        playerBlock = leftBlock;
-        computerBlock = rightBlock;
     }
 };
 var mouseMove = function (e) {
     if (isDragging) {
-        playerBlock.y = e.offsetY;
+        playerBlock.x = e.offsetX;
         clearCanvas();
         draw();
     }
-};
-var computerMove = function (block) {
-    if (gameBall.y > block.y && block.y < (gameBoard.height - block.height)) {
-        block.y++;
-    }
-    else if (gameBall.y < block.y && block.y > 0) {
-        block.y--;
-    }
-    else {
-        return;
-    }
-    clearCanvas();
-    draw();
 };
 gameBoard.onmousedown = mouseDown;
 gameBoard.onmousemove = mouseMove;
@@ -186,17 +124,6 @@ var checkIfBounced = function () {
         var dyChange = (rightBlock.y - gameBall.y) + (rightBlock.height / 2);
         gameBall.dy = -1 * (dyChange / 25);
         rightBlock.incoming = false;
-        leftBlock.incoming = true;
-    }
-    else if (leftBlock.incoming && checkIfHitLeftBlock()) {
-        if (gameBall.dx > -10) {
-            gameBall.dx -= 0.1;
-        }
-        gameBall.dx = -1 * gameBall.dx;
-        var dyChange = (leftBlock.y - gameBall.y) + (leftBlock.height / 2);
-        gameBall.dy = -1 * (dyChange / 25);
-        leftBlock.incoming = false;
-        rightBlock.incoming = true;
     }
     else if (checkIfHitWall()) {
         gameBall.dy = -1 * gameBall.dy;
@@ -217,7 +144,6 @@ var checkIfScored = function () {
         gameBall.x = 250;
         gameBall.dx = 2;
         gameBall.dy = 0;
-        leftBlock.incoming = false;
         rightBlock.incoming = true;
         return true;
     }
@@ -228,9 +154,7 @@ var checkIfScored = function () {
         gameBall.dx = -2;
         gameBall.dy = 0;
         rightBlock.incoming = false;
-        leftBlock.incoming = true;
         return true;
     }
     return false;
 };
-document.addEventListener("keydown", changeDirection);
--- script.ts
+++ script.ts
@@ -9,28 +9,6 @@ let playerBlock
 let computerBlock
 // gameBoard.width = '10%'
 
-let Block: {
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    incoming: boolean
-}
-
-let leftBlock: {
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    incoming: boolean
-} = {
-    x: 50,
-    y: 200,
-    width: 20,
-    height: 100,
-    incoming: false
-}
-
 let rightBlock: {
     x: number,
     y: number,
@@ -38,10 +16,10 @@ let rightBlock: {
     height: number,
     incoming: boolean
 } = {
-    x: 750,
-    y: 200,
-    width: 20,
-    height: 100,
+    x: 350,
+    y: 450,
+    width: 100,
+    height: 20,
     incoming: true
 }
 
@@ -55,8 +33,8 @@ let gameBall: {
     x: 250,
     y: 100,
     radius: 10,
-    dy: 0,
-    dx: 2
+    dy: 2,
+    dx: 0
 }
 
 let scores: {
@@ -81,9 +59,6 @@ function main(): void {
                 draw()
             }, 3000)
         }
-        if(isDragging) {
-            computerMove(computerBlock)
-        }
         gameBall.x += gameBall.dx
         gameBall.y += gameBall.dy
         draw()
@@ -104,48 +79,12 @@ const clearCanvas = (): void => {
 const draw = ():void => {
     gameBoardContext.fillStyle = 'lightblue'
     gameBoardContext.strokeStyle = 'darkblue'
-    gameBoardContext.fillRect(leftBlock.x, leftBlock.y, leftBlock.width, leftBlock.height)
-    gameBoardContext.strokeRect(leftBlock.x, leftBlock.y, leftBlock.width, leftBlock.height); 
-
+   
     gameBoardContext.fillRect(rightBlock.x, rightBlock.y, rightBlock.width, rightBlock.height)
     gameBoardContext.strokeRect(rightBlock.x, rightBlock.y, rightBlock.width, rightBlock.height);
     gameBoardContext.beginPath()
     gameBoardContext.arc(gameBall.x, gameBall.y, gameBall.radius, 0, 2 * Math.PI)
     gameBoardContext.stroke()
-}
-
-const changeDirection = (e): void => {
-    const UP_KEY = 38;
-    const DOWN_KEY = 40;
-    const W_KEY = 87;
-    const S_KEY = 83;
-
-    const keyPressed = e.keyCode
-    if (keyPressed === UP_KEY && rightBlock.y > 0) {
-        rightBlock.y -= 20;
-    }
-    else if (keyPressed === DOWN_KEY && rightBlock.y < (400 - rightBlock.height)) {
-        rightBlock.y += 20;
-    }
-    else if (keyPressed === W_KEY && leftBlock.y > 0) {
-        leftBlock.y -= 20;
-    }
-    else if (keyPressed === S_KEY && leftBlock.y < (400 - leftBlock.height)) {
-        leftBlock.y += 20;
-    }
-    clearCanvas();
-    draw();
-}
-
-const checkIfHitLeftBlock = (): boolean => {
-    if (
-        ((gameBall.x - gameBall.radius) <= (leftBlock.x + leftBlock.width) && (gameBall.x - gameBall.radius) >= (leftBlock.x))
-        && ((gameBall.y + gameBall.radius) > leftBlock.y && (gameBall.y - gameBall.radius) < (leftBlock.y + 100))
-    ) {
-        return true
-    } else {
-        return false
-    }
 }
 
 const checkIfHitRightBlock = (): boolean => {
@@ -197,32 +136,15 @@ let mouseDown = function(e) {
     if(checkIfInBlock(startX, startY, rightBlock)) {
         isDragging = true
         playerBlock = rightBlock
-        computerBlock = leftBlock
-    } else if (checkIfInBlock(startX, startY, leftBlock)) {
-        isDragging = true
-        playerBlock = leftBlock
-        computerBlock = rightBlock
     }
 }
 
 let mouseMove = function(e) {
     if (isDragging) {
-        playerBlock.y = e.offsetY
+        playerBlock.x = e.offsetX
         clearCanvas()
         draw()
     }
-}
-
-const computerMove = (block): void => {
-    if(gameBall.y > block.y && block.y < (gameBoard.height - block.height)) {
-        block.y++
-    } else if (gameBall.y < block.y && block.y > 0) {
-        block.y--
-    } else {
-        return
-    }
-    clearCanvas()
-    draw()
 }
 
 gameBoard.onmousedown = mouseDown
@@ -237,16 +159,6 @@ const checkIfBounced = (): void => {
         let dyChange = (rightBlock.y - gameBall.y) + (rightBlock.height/2)
         gameBall.dy = -1*(dyChange/25)
         rightBlock.incoming = false
-        leftBlock.incoming = true
-    } else if (leftBlock.incoming && checkIfHitLeftBlock()) {
-        if (gameBall.dx > -10) {
-            gameBall.dx -= 0.1
-        }
-        gameBall.dx = -1*gameBall.dx
-        let dyChange = (leftBlock.y - gameBall.y) + (leftBlock.height/2)
-        gameBall.dy = -1*(dyChange/25)
-        leftBlock.incoming = false
-        rightBlock.incoming = true
     } else if (checkIfHitWall()) {
         gameBall.dy = -1*gameBall.dy
     }
@@ -268,7 +180,6 @@ const checkIfScored = (): boolean => {
         gameBall.x = 250
         gameBall.dx = 2
         gameBall.dy = 0
-        leftBlock.incoming = false
         rightBlock.incoming = true
         return true
     } else if ((gameBall.x + gameBall.radius) >= gameBoard.width) {
@@ -278,10 +189,7 @@ const checkIfScored = (): boolean => {
         gameBall.dx = -2
         gameBall.dy = 0
         rightBlock.incoming = false
-        leftBlock.incoming = true
         return true
     }
     return false
 }
-
-document.addEventListener("keydown", changeDirection)
